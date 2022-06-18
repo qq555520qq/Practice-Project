ca24e34e88fa77de6da2d9f661cf602cd2cae7d9
GeneChing <geneching@hiwin.tw>
2022-06-18 10:35:28 +0800

    #developing 刪除Condition

--- src/letterGrade.java
+++ src/letterGrade.java
@@ -9,12 +9,6 @@ public class letterGrade {
         char grade;
         if (score < 0 || score > 100) {
             grade = 'X';
-        } else if (90 <= score && score <= 100) {
-            grade = 'A';
-        } else if (80 <= score && score < 90){
-            grade = 'B';
-        }else if(70<=score &&score<80){
-            grade ='C';
         }else if(60 <= score && score <70){
             grade = 'D';
         }else{
